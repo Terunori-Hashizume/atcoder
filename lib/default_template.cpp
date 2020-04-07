ffa5ac6f97661148e7f364d245d9061f6ede4be7
Terunori Hashizume <hashizume.terunori@gmail.com>
2020-04-07 20:21:33 +0900

    added type aliases and macros to the cpp template file

--- lib/default_template.cpp
+++ lib/default_template.cpp
@@ -1,9 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
+// using block
+#pragma region using
+using ll=long long;
+using vi=vector<int>;
+using vl=vector<ll>;
+using vs=vector<string>;
+using ii=pair<int,int>;
+#pragma endregion
 
-int main() {
+// macro block
+#pragma region macro
+#define INF 2e9
+#define REP(i,n) for(int i=0;i<(int)(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(int)(b);++i)
+#define ALL(v) (v).begin(),(v).end()
+#define MAX(v) *max_element(ALL(v))
+#define MIN(v) *min_element(ALL(v))
+#define SZ(v) ((ll)(v).size())
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#pragma endregion
+
+const ll MOD = 1e9 + 7;
+
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
